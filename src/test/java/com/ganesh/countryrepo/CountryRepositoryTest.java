f53f8a224c378b0762446c87c0ebdb8c2011c7a7
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-03-02 19:00:04 +0530

    # adding seprate testing properties file

--- src/test/java/com/ganesh/countryrepo/CountryRepositoryTest.java
+++ src/test/java/com/ganesh/countryrepo/CountryRepositoryTest.java
@@ -12,12 +12,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest(showSql = false)
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(locations = "classpath:test.properties")
 public class CountryRepositoryTest {
 
     @Autowired
